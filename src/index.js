a033eff201419fd69515512879325beddbd6a8bd
Tushar Jain <tjain210@gmail.com>
2023-04-24 22:55:02 +0530

    Added login screen

--- src/index.js
+++ src/index.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import './index.css';
-import App from './App';
-import Sidebar from './components/Sidebar';
-
+import "./index.css";
+import App from "./App";
+import Sidebar from "./components/Sidebar";
+import Login from "./routes/Login";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Sidebar />,
+    element: <Login />,
   },
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <RouterProvider router={router} />
-);
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={router} />);
