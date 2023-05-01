de4e27a411470f3ee91381eedbfa4862d8c0dc8b
Tushar Jain <tjain210@gmail.com>
2023-05-01 23:44:47 +0530

    added cloudinary

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,14 @@ import UserDetails from "./routes/UserDetails";
 import GroupsJoinedList from "./components/GroupsJoinedList";
 import UserList from "./components/UserList";
 import PostDetails from "./components/PostDetails";
+import Upload from "./components/Upload";
 
 function App() {
   return (
     <div className="min-h-screen max-h-screen flex flex-auto flex-shrink-0 antialiased bg-gray-50">
       <Sidebar />
       <div id="detail" className="max-h-screen overflow-hidden w-full">
-        <Outlet />
+        <Upload />
       </div>
     </div>
   );
